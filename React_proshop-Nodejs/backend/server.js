506bac0227911d6fa20e39e995b007ca8a20dd52
vanitaperumandla <vanitaperu>
2024-08-01 11:24:50 -0400

    My First commit
    Final Project

---
+++ React_proshop-Nodejs/backend/server.js
@@ -0,0 +1,45 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import cookieParser from "cookie-parser";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB(); //Connect to MongoDB
+
+const app = express();
+
+//Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//Cookie parser middleware
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+const __dirname = path.resolve(); //set path to curent directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
